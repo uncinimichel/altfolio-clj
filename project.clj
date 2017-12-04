7feec0b90140e9bdd6977cbfb5b50d266eb73079
Michel Uncini <michel.uncini@thomsonreuters.com>
2017-12-04 16:03:49 +0000

    Second commit

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject altfolio-clj "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0-alpha16"]
+                 [org.clojure/core.async "0.3.442"]
+                 [clj-http "3.7.0"]
+                 [org.clojure/data.json "0.2.6"]
+                 [org.clojure/test.check "0.9.0"]])
